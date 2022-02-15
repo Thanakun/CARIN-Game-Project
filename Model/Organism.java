d7e493625a04353d099c97f34bc8da5be7e9f2ba
Chanchai <monnoi.92@gmail.com>
2022-02-15 14:34:08 +0700

    add Move and Attack method in Organism

--- Model/Organism.java
+++ Model/Organism.java
@@ -1,8 +1,12 @@
 package Model;
+import Parser.Grammars.Direction;
+
 import java.util.Map;
 
 public interface Organism {
     void Show_Status();
     int HP();
     Map Position();
+    void Move(Direction direction);
+    void Attack(Direction direction);
 }

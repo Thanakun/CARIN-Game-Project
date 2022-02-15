6129913e863447ffe86d34f84dc15a491c97d6a0
Chanchai <monnoi.92@gmail.com>
2022-02-15 11:47:24 +0700

    modified to consider blank space

--- Parser/Tokenizer.java
+++ Parser/Tokenizer.java
@@ -1,10 +1,10 @@
 package Parser;
-
+import java.util.regex.Pattern;
 
 public class Tokenizer {
-    protected String src;
-    protected String next;
-    protected int pos;
+    private String src;
+    private String next;
+    private int pos;
 
     public Tokenizer(String src){
         this.src = src;
@@ -12,61 +12,91 @@ public class Tokenizer {
         computeNext();
     }
 
-    private void computeNext() {
-        StringBuilder s = new StringBuilder();
-        while (pos < src.length() && Character.isWhitespace(src.charAt(pos))) pos++;  // ignore whitespace
-        while (pos < src.length()) {
-            char c = src.charAt(pos);
-            if (Character.isDigit(c)) {  // start of number
-                s.append(c);
-                for (; pos < src.length() && Character.isDigit(src.charAt(pos)); pos++)
-                    s.append(src.charAt(pos));
-                break;
-            } else if (Character.isLetter(c)) {  // start of identifier
-                s.append(c);
-                pos++;
-                break;
-            }
-            else if(c == ' '){
-                pos++;
-            }
-            else if (c == '+' || c== '-' || c == '(' || c == ')' || c == '*' || c == '/' || c == '%' || c== '{' || c == '}' || c == '^' || c == '=' || c=='_') {
-                s.append(c);
-                pos++;
-                break;
-            } else throw new SyntaxError("Unknown : " + c);
+    private void computeNext(){
+        StringBuilder sub = new StringBuilder();
+
+        while(pos<src.length() && ((src.charAt(pos)==' ')||(src.charAt(pos)=='\n'))){  //skip blank space
+            next = " ";
+            pos++;
+            return;
         }
-        next = s.toString();
+        if(pos==src.length()){  //if blank space until end of string
+            next = "";
+            return;
+        }
+        char ch = src.charAt(pos);
+        if(Character.isDigit(ch)){  //start number
+            sub.append(ch);
+            pos++;
+            for(;pos<src.length() && Character.isDigit(src.charAt(pos));pos++){
+                sub.append(src.charAt(pos));
+            }
+        }
+        else if(ch=='+'||ch=='-'||ch=='*'||ch=='/'||ch=='%'||ch=='('||ch==')'||ch=='='||ch=='{'||ch=='}'||ch=='^')  //operator
+        {
+            sub.append(ch);
+            pos++;
+        }
+        else if(isCharacter(ch)){ //start identifier or reserved word
+            sub.append(ch);
+            pos++;
+            for(;pos<src.length()
+                    &&(Character.isDigit(src.charAt(pos)) || isCharacter(src.charAt(pos)));pos++){  //after first character can be a-zA-z0-9_
+                sub.append(src.charAt(pos));
+            }
+        }
+        else{
+            throw  new RuntimeException("Unknown character: "+ch);
+        }
 
+        next = sub.toString();
     }
 
+    //check if ch is a-z,A-Z,_
+    private boolean isCharacter(char ch){
+        return Pattern.matches("[a-zA-z]",new StringBuilder(1).append(ch));
+    }
 
+    /**Returns next token in the input stream**/
+    public String peek(){
+        while(next==" "){
+            computeNext();
+        }
+        return next;
+    }
+    /**Returns true if the next token is s **/
     public boolean peek(String s){
-        if(!src.isEmpty()) return peek().equals(s);
-        return false;
+        while(next==" "){
+            computeNext();
+        }
+        return peek().equals(s);
     }
-
-    public void consume(String s) throws SyntaxError {
-        if(peek(s)) {
-            consume();
-        }else
-            throw new SyntaxError();
-    }
-
-
-    public boolean hasNext(){
-        return !next.equals("");
-    }
-
-    public String peek() {
-        return next; }
-
+    /**Consumes the next token from input stream and return it**/
     public String consume(){
+        while(next==" "){
+            computeNext();
+        }
         String result = next;
-        computeNext();
-        return result;
+        if(pos<src.length()){
+            computeNext();
+            return result;
+        }
+        else {   //last token
+            next = "";
+            return result;
+        }
     }
-
-
-
+    /**Consume the next token if it is s
+     * Throw syntaxError otherwise**/
+    public void consume(String s) throws SyntaxError{
+        while(next==" "){
+            computeNext();
+        }
+        if(peek(s)){
+            consume();
+        }
+        else{
+            throw new SyntaxError();
+        }
+    }
 }

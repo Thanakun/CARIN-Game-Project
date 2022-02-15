b3287c7dfd09f3e2e9eba7bbf1e1d285c5ac65a2
Chanchai <monnoi.92@gmail.com>
2022-02-15 09:41:19 +0700

    add Identifier and Number class

---
+++ Parser/Grammars/Identifier.java
@@ -0,0 +1,19 @@
+package Parser.Grammars;
+
+import java.util.Map;
+
+public class Identifier implements Expression{
+    private String name;
+    public Identifier(String name){
+        this.name = name;
+    }
+    @Override
+    public int eval(Map<String, Expression> binding) {
+        return 0;
+    }
+
+    @Override
+    public void prettyPrint(StringBuilder s) {
+
+    }
+}
---
+++ Parser/Grammars/Number.java
@@ -0,0 +1,19 @@
+package Parser.Grammars;
+
+import java.util.Map;
+
+public class Number implements Expression{
+    private int value;
+    public  Number(int value){
+        this.value = value;
+    }
+    @Override
+    public int eval(Map<String, Expression> binding) {
+        return 0;
+    }
+
+    @Override
+    public void prettyPrint(StringBuilder s) {
+
+    }
+}

b893757f70f5a16aac0fb0bd3c1043c941661744
Chanchai <monnoi.92@gmail.com>
2022-02-16 23:09:14 +0700

    -add react app

---
+++ SpringData/game/src/main/java/server/game/Game/GameService.java
@@ -0,0 +1,11 @@
+package server.game.Game;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameService {
+
+    public String getHello() {
+        return "Hello";
+    }
+}

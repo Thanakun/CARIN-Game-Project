7198288141215a31c8618bc1294fdc1308941ebe
Chanchai <monnoi.92@gmail.com>
2022-02-16 22:22:56 +0700

    -add spring project data

---
+++ SpringData/game/src/test/java/server/game/GameApplicationTests.java
@@ -0,0 +1,13 @@
+package server.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
